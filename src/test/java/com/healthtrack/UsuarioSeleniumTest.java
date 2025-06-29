68ae25762e6a5a49b101f66183c685f6e0ae3474
GeraldOpitz <geraldopitz@gmail.com>
2025-06-29 06:38:52 -0400

    selenium ci

--- src/test/java/com/healthtrack/UsuarioSeleniumTest.java
+++ src/test/java/com/healthtrack/UsuarioSeleniumTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,7 +18,16 @@ public class UsuarioSeleniumTest {
     @BeforeEach
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
     }
 
     @Test
